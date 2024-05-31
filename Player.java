04682dee5dc4efb2ccd769c4a9cdd267d42d4ed3
Adrian400811 <adrianlee@adrian40.xyz>
2024-05-31 11:48:09 -0400

    Fixed player going out of bound Reduced hardcoded values

--- Player.java
+++ Player.java
@@ -8,33 +8,51 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Player extends Actor
 {
+    private World w;
     private static int speed;
+    public void addedToWorld(World w){
+        this.w = w;
+    }
+    
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
-    {
+    {   
+        // Movement
         if (Greenfoot.isKeyDown("D")){
             setLocation(getX()+2, getY());
             speed = 2;
-        } if (Greenfoot.isKeyDown("A")){
+        }
+        if (Greenfoot.isKeyDown("A")){
             setLocation(getX()-2, getY());
             speed = -2;
-        } if (Greenfoot.isKeyDown("Space") && isTouching(Block.class)){
+        }
+        // Jump
+        if (Greenfoot.isKeyDown("Space") && isTouching(Block.class)){
             for (int i = 0; i < 20; i+=2){
                 setLocation(getX(), getY()-i);
             }
-        } if (!isTouching(Block.class)){
+        }
+        // Fall
+        if (!isTouching(Block.class)){
             setLocation(getX(), getY()+2);
-        } if (getX() < 0){
-            setLocation(0,getY());
-        } if (getX() > 2560){
-            setLocation(2560, getY());
-        } if (getY() < 0){
-            setLocation(getX(), 0);
-        } if (getY() > 720){
-            setLocation(getX(), 720);
+        }
+        // Boundary
+        if(w != null){
+            if (getX() < 0){
+                setLocation(0,getY());
+            }
+            if (getX() > w.getWidth()){
+                setLocation(w.getWidth(), getY());
+            }
+            if (getY() < 0){
+                setLocation(getX(), 0);
+            }
+            if (getY() > w.getHeight()){
+                setLocation(getX(), w.getHeight());
+            }
         }
     }
     

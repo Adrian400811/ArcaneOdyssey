24e30478037399d7bb7936d908fa1f34782bee9c
Adrian400811 <adrianlee@adrian40.xyz>
2024-06-03 14:39:51 -0400

    Formatting

--- MyWorld.java
+++ MyWorld.java
@@ -2,25 +2,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class MyWorld here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class MyWorld extends World
-{
-    private ImgScroll scroll;
-    private Player player;
+public class MyWorld extends World {
+    private final ImgScroll scroll;
+    private final Player player;
     private int[][] blockGeneration;
     private Orb orb;
 
     /**
      * Constructor for objects of class MyWorld.
-     * 
      */
-    public MyWorld()
-    {    
+    public MyWorld() {
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1280, 720, 1, false); 
+        super(1280, 720, 1, false);
         addObject(player = new Player(), 100, 622);
         scroll = new ImgScroll(this, new GreenfootImage("2dPixelForestBackground.png"), 2560, 720);
         // Flooring
@@ -41,33 +38,33 @@ public class MyWorld extends World
         blockGeneration[5][7] = 1;
         spawnTerrain(blockGeneration);
     }
-    
-    
-    public void act(){
-        scroll.scroll(getWidth()/2-player.getX(), getHeight()/2-player.getY());
+
+
+    public void act() {
+        scroll.scroll(getWidth() / 2 - player.getX(), getHeight() / 2 - player.getY());
         checkNext();
     }
-    
+
     /**
      * NOTE - Use a 2d array of [40][10] for this to work as intended
      * Each value in the array represents 64x and 72y
      */
-    public void spawnTerrain(int[][] identifier){
-        for (int i = 0; i < identifier.length; i++){
-            for (int j = 0; j < identifier[i].length; j++){
-                if (identifier[i][j] == 1){
+    public void spawnTerrain(int[][] identifier) {
+        for (int i = 0; i < identifier.length; i++) {
+            for (int j = 0; j < identifier[i].length; j++) {
+                if (identifier[i][j] == 1) {
                     // i represents the X-values and j represents the y-values
-                    addObject(new Brick(), i*64, j*72);
-                } 
-                if (identifier[i][j] == 2){
-                    addObject(orb = new Orb(), i*64, j*72);
+                    addObject(new Brick(), i * 64, j * 72);
+                }
+                if (identifier[i][j] == 2) {
+                    addObject(orb = new Orb(), i * 64, j * 72);
                 }
             }
         }
     }
-    
-    public void checkNext(){
-        if (orb.isBeingTouched()){
+
+    public void checkNext() {
+        if (orb.isBeingTouched()) {
             Level1 world = new Level1();
             Greenfoot.setWorld(world);
         }
--- Player.java
+++ Player.java
@@ -1,17 +1,23 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.Actor;
+import greenfoot.Greenfoot;
+import greenfoot.World;
 
 /**
  * Write a description of class Player here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Player extends Actor
-{
-    private World w;
+public class Player extends Actor {
     private static int speed;
+    private World w;
     private int jumpActs = 0;
-    public void addedToWorld(World w){
+
+    public static int getSpeed() {
+        return speed;
+    }
+
+    public void addedToWorld(World w) {
         this.w = w;
     }
 
@@ -19,68 +25,62 @@ public class Player extends Actor
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
+    public void act() {
         jumpActs--;
         // Movement
-        if (Greenfoot.isKeyDown("D")){
-            setLocation(getX()+8, getY());
+        if (Greenfoot.isKeyDown("D")) {
+            setLocation(getX() + 8, getY());
             speed = 8;
         }
-        if (Greenfoot.isKeyDown("A")){
-            setLocation(getX()-8, getY());
+        if (Greenfoot.isKeyDown("A")) {
+            setLocation(getX() - 8, getY());
             speed = -8;
         }
         // Jump
-        if (Greenfoot.isKeyDown("Space") && isTouching(Brick.class)){
+        if (Greenfoot.isKeyDown("Space") && isTouching(Brick.class)) {
             jumpActs = 30;
         }
         if (jumpActs!=0){
             if (jumpActs > 15){
                 setLocation(getX(), getY() - 10);
-                    //If Player hits head on Brick it will end the jump motion
-                    if (getOneIntersectingObject(Brick.class) != null){
-                        setLocation(getX(), getY() + 10);
-                        jumpActs = 15;
-                    }
+                //If Player hits head on Brick it will end the jump motion
+                if (getOneIntersectingObject(Brick.class) != null){
+                    setLocation(getX(), getY() + 10);
+                    jumpActs = 15;
+                }
             }
             if (jumpActs < 15 && !isTouching(Brick.class)){
                 setLocation(getX(), getY() + 8);
             }
-        }
         // Gravity
-        if (!isTouching(Brick.class)){
-            setLocation(getX(), getY()+2);
+        if (!isTouching(Brick.class)) {
+            setLocation(getX(), getY() + 2);
         }
         // Boundary
-        if(w != null){
-            if (getX() < 0){
-                setLocation(0,getY());
+        if (w != null) {
+            if (getX() < 0) {
+                setLocation(0, getY());
             }
-            if (getX() > w.getWidth()){
+            if (getX() > w.getWidth()) {
                 setLocation(w.getWidth(), getY());
             }
-            if (getY() < 0){
+            if (getY() < 0) {
                 setLocation(getX(), 0);
             }
-            if (getY() > w.getHeight()){
+            if (getY() > w.getHeight()) {
                 setLocation(getX(), w.getHeight());
             }
         }
         // Collision Detection
-        if (getOneObjectAtOffset(getImage().getWidth()/2, 0, Brick.class) != null){
-            setLocation(getX()-speed, getY());
+        if (getOneObjectAtOffset(getImage().getWidth() / 2, 0, Brick.class) != null) {
+            setLocation(getX() - speed, getY());
         }
-        if (getOneObjectAtOffset(-(getImage().getWidth()/2), 0, Brick.class) != null){
-            setLocation(getX()-speed, getY());
+        if (getOneObjectAtOffset(-(getImage().getWidth() / 2), 0, Brick.class) != null) {
+            setLocation(getX() - speed, getY());
         }
-    }
-    
-    public static int getSpeed(){
-        return speed;
     }
 
-    public int[] getLocation(){
+    public int[] getLocation() {
         int[] location = new int[2];
         location[0] = getX();
         location[1] = getY();

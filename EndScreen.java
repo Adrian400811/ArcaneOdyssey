7e5a0b26ab5adb4a61c81fb528bf70427ac08361
349928465 <349928465@gapps.yrdsb.ca>
2024-06-13 11:23:43 -0400

    Added some text to EndScreen

--- EndScreen.java
+++ EndScreen.java
@@ -8,7 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class EndScreen extends World
 {
+    private final Font font = new Font("Arial", 64);
+    private final SuperDisplayLabel titleLabel = new SuperDisplayLabel(Color.WHITE, Color.BLACK, font);
+    private final GreenfootImage titleImage = new GreenfootImage("youWin.png");
 
+    private final SuperDisplayLabel gameOverLabel = new SuperDisplayLabel(font);
+    private final GreenfootImage gameOverImage = new GreenfootImage("gameOverInstructions.png");
+
+    private Crown crown = new Crown();
     /**
      * Constructor for objects of class EndScreen.
      * 
@@ -20,5 +27,32 @@ public class EndScreen extends World
 
         // Background
         setBackground("brick.jpg");
+        
+        // Title
+        addObject(titleLabel, 600, 200);
+        titleLabel.setImage(titleImage);
+        titleLabel.setLocation(640, 150);
+        
+        // Instruction Label
+        addObject(gameOverLabel, getWidth() / 2, 600);
+        gameOverLabel.update("Press 'enter' to go to the Title Screen");
+        gameOverImage.scale(1100, 60);
+        gameOverLabel.setImage(gameOverImage);
+        gameOverLabel.setLocation(getWidth() / 2, 600);
+        
+        // Add a Crown 
+        addObject(crown, getWidth()/2, getHeight()/2);
+
+    }
+    
+    public void act() {
+        checkKeys();
+    }
+
+    private void checkKeys() {
+        if (Greenfoot.isKeyDown("enter")) {
+            TitleScreen title = new TitleScreen();
+            Greenfoot.setWorld(title);
+        }
     }
 }
